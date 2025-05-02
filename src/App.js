59a78f9fc940fe78d7cf0be4760a777ddc99a3c6
Piotr Lecicki <p.lecicki@gmail.com>
2025-05-02 13:52:12 +0200

    Project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {/*<NavBar />*/}
+      {/*<Container>*/}
+      {/*  <Routes>*/}
+          {/*<Route path="*" element={<PageTitle>404 NOT FOUND</PageTitle>} />*/}
+          {/*<Route path="/" element={<Home />} />*/}
+          {/*<Route path="/about" element={<About />} />*/}
+          {/*<Route path="/favorite" element={<Favorite />} />*/}
+          {/*<Route path="/list/:listId" element={<List />} />*/}
+      {/*  </Routes>*/}
+      {/*</Container>*/}
+    </main>
   );
-}
-
+};
 export default App;
